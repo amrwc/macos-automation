43745f3e8e1359b69dbbba7925d3a44cf4ca8022
amrwc <42574496+amrwc@users.noreply.github.com>
2020-09-14 23:23:57 +0200

    Remove old code

--- bluetooth.py
+++ bluetooth.py
@@ -40,38 +40,6 @@ from utils import (
 )
 
 
-# def main(argv: List[str] = []) -> None:
-#     """
-#     The application's entry point.
-#     """
-#     if not argv:
-#         argv = sys.argv[1:]
-#     if len(argv) == 0:
-#         raise_error('No option provided', usage=usage)
-#     option: str = argv[0]
-#     if option not in ('on', 'off',):
-#         raise_error(f"Unsupported option provided: {option}", usage=usage)
-#     set_bluetooth(option == 'on')
-
-
-# def set_bluetooth(on: bool) -> None:
-#     """
-#     Runs a `blueutil` command and turns Bluetooth to the given state.
-#     @param on: whether to turn Bluetooth on
-#     """
-#     power: str = '1' if on else '0'
-#     log(f"Turning Bluetooth {'on' if on else 'off'}")
-#     execute_cmd(['blueutil', '-p', power])
-
-
-# def usage() -> None:
-#     """
-#     Prints usage instructions.
-#     """
-#     print_coloured('Usage:\n', 'white', 'bold')
-#     print_coloured('$ ./bluetooth.py <on/off>\n', 'white')
-
-
 class Bluetooth(Automation):
 
     def __init__(self, argv: List[str] = []):
