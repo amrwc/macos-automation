9177f5e27793532676a48c64bfb4c1df3afc69d4
amrwc <42574496+amrwc@users.noreply.github.com>
2020-09-14 23:24:54 +0100

    Rewrite `tunnelblick` using `abstract_automation`

--- tests/test_tunnelblick.py
+++ tests/test_tunnelblick.py
@@ -7,13 +7,10 @@ from testing_utils import (
     next_alphabetic,
     next_alphanumeric,
 )
-from tunnelblick import (
-    main,
-    parse_argv,
-    get_config,
-    build_apple_script,
-    usage,
+from testing_automation_common import (
+    mock_parse_argv,
 )
+from tunnelblick import Tunnelblick
 
 
 MODULE_NAME = 'tunnelblick'
@@ -22,94 +19,93 @@ CONFIG: Dict[str, str] = {
 }
 
 
-@pytest.mark.parametrize('argv, raised_error', [
-    (['connect'], True),
-    (['quit'], True),
-    (['connect'], False),
-    (['quit'], False),
+@pytest.mark.parametrize('argv', [
+    (['connect']),
+    (['quit']),
 ])
-def should_have_run_uninterrupted(monkeypatch, argv: List[str], raised_error: bool) -> None:
+def should_have_executed(monkeypatch, argv: List[str]) -> None:
     def mock_execute_cmd(*args: tuple, **kwargs: dict) -> str:
         assert args[0] == ['osascript', '-e', apple_script]
-        return stdout
+        return 'true\n'
     apple_script: str = next_alphanumeric(20)
-    stdout: str = 'true\n'
-    raise_error_calls: list = []
-    if raised_error:
-        stdout = next_alphanumeric(10)
-        monkeypatch.setattr(f"{MODULE_NAME}.raise_error", lambda *a, **k: raise_error_calls.append(''))
-    monkeypatch.setattr(f"{MODULE_NAME}.parse_argv", lambda *a, **k: argv)
-    monkeypatch.setattr(f"{MODULE_NAME}.get_config", lambda *a, **k: CONFIG)
-    monkeypatch.setattr(f"{MODULE_NAME}.build_apple_script", lambda *a, **k: apple_script)
+    mock_parse_argv(MODULE_NAME, 'Tunnelblick', monkeypatch, argv)
+    monkeypatch.setattr(f"{MODULE_NAME}.Tunnelblick.get_config", lambda *a, **k: CONFIG)
+    monkeypatch.setattr(f"{MODULE_NAME}.Tunnelblick.build_apple_script", lambda *a, **k: apple_script)
     monkeypatch.setattr(f"{MODULE_NAME}.execute_cmd", mock_execute_cmd)
     mute_logs(MODULE_NAME, monkeypatch)
-    main()
-    assert len(raise_error_calls) == (1 if raised_error else 0)
+    result: str = Tunnelblick().execute()
+    assert result == 'true'
 
 
-@pytest.mark.parametrize('argv, expected_result', [
-    ([f"{MODULE_NAME}.py"], None),
-    ([f"{MODULE_NAME}.py", next_alphabetic(10)], None),
+@pytest.mark.parametrize('argv', [
+    ([next_alphabetic(10)]),
+    ([]),
 ])
-def should_have_raised_error_when_parsing_argv(monkeypatch, argv: List[str], expected_result: List[str]) -> None:
+def should_not_have_parsed_argv_with_wrong_or_no_option(monkeypatch, argv: List[str]) -> None:
     def mock_raise_error(*args: tuple, **kwargs: dict) -> None:
-        assert kwargs['usage'] == usage
+        assert kwargs['usage'] is not None
         raise SystemExit(0)  # Controlled early exit
     monkeypatch.setattr(f"{MODULE_NAME}.raise_error", mock_raise_error)
 
     with pytest.raises(SystemExit) as e:
-        assert parse_argv(argv) == expected_result
+        Tunnelblick(argv)
     assert e.type == SystemExit
     assert e.value.code == 0
 
 
-@pytest.mark.parametrize('argv, expected_result', [
-    ([f"{MODULE_NAME}.py", 'connect'], ['connect']),
-    ([f"{MODULE_NAME}.py", 'quit'], ['quit']),
+@pytest.mark.parametrize('argv', [
+    (['connect']),
+    (['quit']),
 ])
-def should_have_parsed_argv(monkeypatch, argv: List[str], expected_result: List[str]) -> None:
-    assert parse_argv(argv) == expected_result
+def should_have_parsed_argv(monkeypatch, argv: List[str]) -> None:
+    tunnelblick: Tunnelblick = Tunnelblick(argv)
+    assert tunnelblick.argv == argv
+
+
+def should_have_printed_usage_instructions(monkeypatch) -> None:
+    print_coloured_calls: list = []
+    mock_parse_argv(MODULE_NAME, 'Tunnelblick', monkeypatch)
+    monkeypatch.setattr(f"{MODULE_NAME}.print_coloured", lambda *a, **k: print_coloured_calls.append(''))
+    Tunnelblick().usage()
+    assert len(print_coloured_calls) == 2
 
 
 def should_not_have_gotten_config_when_the_file_doesnt_exist(monkeypatch) -> None:
-    def mock_isfile(*args: tuple, **kwargs: dict) -> None:
-        assert args == (config_path,)
+    def mock_isfile(*args: tuple, **kwargs: dict) -> bool:
+        assert args[0] == config_path
         return False
 
     def mock_raise_error(*args: tuple, **kwargs: dict) -> None:
         assert config_path in args[0]
         raise SystemExit(0)  # Controlled early exit
 
+    config_path: str = next_alphabetic(16)
+    mock_parse_argv(MODULE_NAME, 'Tunnelblick', monkeypatch)
     monkeypatch.setattr(f"{MODULE_NAME}.os.path.isfile", mock_isfile)
     monkeypatch.setattr(f"{MODULE_NAME}.raise_error", mock_raise_error)
 
-    config_path: str = next_alphabetic(16)
     with pytest.raises(SystemExit) as e:
-        get_config(config_path)
+        Tunnelblick().get_config(config_path)
     assert e.type == SystemExit
     assert e.value.code == 0
 
 
 def should_have_gotten_config(monkeypatch) -> None:
-    def mock_isfile(*args: tuple, **kwargs: dict) -> None:
-        assert args == (config_path,)
+    def mock_isfile(*args: tuple, **kwargs: dict) -> bool:
+        assert args[0] == config_path
         return True
-    monkeypatch.setattr(f"{MODULE_NAME}.os.path.isfile", mock_isfile)
-
     config_path: str = next_alphabetic(16)
+    mock_parse_argv(MODULE_NAME, 'Tunnelblick', monkeypatch)
+    monkeypatch.setattr(f"{MODULE_NAME}.os.path.isfile", mock_isfile)
+    monkeypatch.setattr(f"{MODULE_NAME}.json.load", lambda *a, **k: CONFIG)
     with patch('builtins.open', mock_open(read_data='data')):
-        monkeypatch.setattr(f"{MODULE_NAME}.json.load", lambda *a, **k: CONFIG)
-        assert get_config(config_path) == CONFIG
+        result: Dict[str, str] = Tunnelblick().get_config(config_path)
+        assert result == CONFIG
 
 
-def should_have_built_apple_script() -> None:
+def should_have_built_apple_script(monkeypatch) -> None:
     instruction: str = next_alphanumeric(10)
+    mock_parse_argv(MODULE_NAME, 'Tunnelblick', monkeypatch)
     expected_result: str = '\n'.join(('tell application "/Applications/Tunnelblick.app"', instruction, 'end tell'))
-    assert build_apple_script(instruction) == expected_result
-
-
-def should_have_printed_usage_instructions(monkeypatch) -> None:
-    print_coloured_calls: list = []
-    monkeypatch.setattr(f"{MODULE_NAME}.print_coloured", lambda *a, **k: print_coloured_calls.append(''))
-    usage()
-    assert len(print_coloured_calls) == 2
+    result: str = Tunnelblick().build_apple_script(instruction)
+    assert result == expected_result
--- tunnelblick.py
+++ tunnelblick.py
@@ -34,6 +34,8 @@ import json
 import os
 import sys
 from typing import Dict, List
+
+from abstract_automation import Automation
 from utils import (
     execute_cmd,
     log,
@@ -47,65 +49,54 @@ SCRIPT_PARENT_DIR_PATH: str = os.path.dirname(os.path.realpath(sys.argv[0]))
 CONFIG_FILE_PATH: str = f"{SCRIPT_PARENT_DIR_PATH}/{CONFIG_FILE_NAME}"
 
 
-def main(argv: List[str] = []) -> None:
-    """
-    The application's entry point.
-    """
-    argv = parse_argv((argv if argv else sys.argv))
-    config: Dict[str, str] = get_config(CONFIG_FILE_PATH)
-    instructions: Dict[str, str] = {
-        'connect': f"connect \"{config['configuration-name']}\"",
-        'quit': 'quit',
-    }
-    instruction: str = instructions[argv[0]]
-    apple_script: str = build_apple_script(instruction)
-    log(f"Instructing Tunnelblick to {instruction}")
-    stdout: str = execute_cmd(['osascript', '-e', apple_script])
-    if stdout not in ('true\n', '0\n'):
-        raise_error(f"Something went wrong; stdout: {stdout}")
+class Tunnelblick(Automation):
 
+    def __init__(self, argv: List[str] = []):
+        self.argv = self.parse_argv(argv)
 
-def parse_argv(argv: List[str]) -> List[str]:
-    """
-    Parses and verifies the command-line arguments and returns them with the script name removed.
-    @return: command-line arguments
-    """
-    argv = argv[1:]
-    if len(argv) == 0:
-        raise_error('No option provided', usage=usage)
-    if argv[0] not in ('connect', 'quit'):
-        raise_error(f"Unsupported option provided: {argv[0]}", usage=usage)
-    return argv
+    def execute(self) -> str:
+        config: Dict[str, str] = self.get_config(CONFIG_FILE_PATH)
+        instructions: Dict[str, str] = {
+            'connect': f"connect \"{config['configuration-name']}\"",
+            'quit': 'quit',
+        }
+        instruction: str = instructions[self.argv[0]]
+        apple_script: str = self.build_apple_script(instruction)
+        log(f"Instructing Tunnelblick to {instruction}")
+        return execute_cmd(['osascript', '-e', apple_script]).strip()
 
+    def parse_argv(self, argv: List[str]) -> List[str]:
+        if len(argv) == 0:
+            raise_error('No option provided', usage=self.usage)
+        if argv[0] not in ('connect', 'quit'):
+            raise_error(f"Unsupported option provided: {argv[0]}", usage=self.usage)
+        return argv
 
-def get_config(config_path: str) -> Dict[str, str]:
-    """
-    Returns the config file contents as a dictionary or raises an error if the file doesn't exist.
-    @param config_path: path to the config file
-    @return: config file contents
-    """
-    if not os.path.isfile(config_path):
-        raise_error(f"The config file doesn't exist in '{config_path}'")
-    with open(config_path) as config_file:
-        return json.load(config_file)
+    def usage(self) -> None:
+        print_coloured('Usage:\n', 'white', 'bold')
+        print_coloured('$ ./tunnelblick.py <connect/quit>\n', 'white')
 
+    def get_config(self, config_path: str) -> Dict[str, str]:
+        """
+        Returns the config file contents as a dictionary or raises an error if the file doesn't exist.
+        @param config_path: path to the config file
+        @return: config file contents
+        """
+        if not os.path.isfile(config_path):
+            raise_error(f"The config file doesn't exist in '{config_path}'")
+        with open(config_path) as config_file:
+            return json.load(config_file)
 
-def build_apple_script(instruction: str) -> str:
-    """
-    Returns a ready-made AppleScript snippet that passes commands into Tunnelblick.
-    @param instruction: command for Tunnelblick
-    @return: AppleScript (osascript) snippet
-    """
-    return '\n'.join(('tell application "/Applications/Tunnelblick.app"', instruction, 'end tell'))
-
-
-def usage() -> None:
-    """
-    Prints usage instructions.
-    """
-    print_coloured('Usage:\n', 'white', 'bold')
-    print_coloured('$ ./tunnelblick.py <connect/quit>\n', 'white')
+    def build_apple_script(self, instruction: str) -> str:
+        """
+        Returns a ready-made AppleScript snippet that passes commands into Tunnelblick.
+        @param instruction: command for Tunnelblick
+        @return: AppleScript (osascript) snippet
+        """
+        return '\n'.join(('tell application "/Applications/Tunnelblick.app"', instruction, 'end tell'))
 
 
 if __name__ == '__main__':
-    main()
+    result: str = Tunnelblick(sys.argv[1:]).execute()
+    if result not in ('true', '0'):
+        raise_error(f"Something went wrong; stdout: {result}")
