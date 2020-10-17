2fa9ba152f2bb110dce066fce17338f4b5ee39c0
amrwc <42574496+amrwc@users.noreply.github.com>
2020-10-17 20:00:32 +0100

    Feature: `appearance`

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ class Automation():
 
 
 AUTOMATIONS = [
+    Automation('appearance', 'appearance'),
     Automation('bluetooth', 'bluetooth'),
     Automation('brightness', 'bright'),
     Automation('goodnight', 'goodnight'),
